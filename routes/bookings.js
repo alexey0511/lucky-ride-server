c48f66f24b5ec855c950400eba86ad4ad4c8954c
alexeyomelchenko <alexeyomelchenko@catalyst.net.nz>
2017-07-26 07:07:50 +1200

    FIRST COMMIT

---
+++ routes/bookings.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+var mongojs = require("mongojs");
+
+var db = mongojs("mongodb://alexey:alexey@ds121483.mlab.com:21483/lucky_ride", ["bookings"])
+router.get("/bookings", function(req, res, next) {
+  db.bookings.find(function(err, bookings) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(bookings);
+  });
+});
+
+module.exports = router;
+
+
+router.post("bookings", function(req, res, next) {
+  var booking = req.body.data;
+
+    if (!booking.userName) {
+      res.status(400);
+      res.json({
+        error: "Bad data"
+      });
+    } else {
+      db.bookings.save(booking, function(err, savedBooking) {
+        if (err) {
+          res.send(err)''
+        }
+        res.json(savedBooking);
+      });
+    }
+})
