6179aa8e67da61b66c351334d0094fe1302b5e02
alexeyomelchenko <alexeyomelchenko@catalyst.net.nz>
2017-07-26 23:06:47 +1200

    add driver location hardcoded

--- server.js
+++ server.js
@@ -4,26 +4,35 @@ var bodyParser = require("body-parser");
 
 var index = require("./routes/index");
 var bookings = require("./routes/bookings");
+var driverLocation = require("./routes/driverLocation");
 
 var app = express();
 
-var port = Number(process.env.PORT || 5001);
-app.listen(port, function() {
-  console.log('Server running on port', port)
-});
+var port = 3001;
+
+var socket_io = require("socket.io");
+
+var io = socket_io();
+
 
 //views
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views",  path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 //Body parser MW
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended:true}));
 
-// routes
+
+//Routes
 
 app.use("/", index);
 app.use("/api", bookings);
+app.use("/api", driverLocation);
+
+io.listen(app.listen(port, function(){
+	console.log("Server running on port", port);
+}));
