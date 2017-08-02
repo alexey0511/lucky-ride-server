bd9fd995d436fcb862e98bd0f2d4bbc77971fd4a
alexey0511 <alexey0511@users.noreply.github.com>
2017-08-02 22:37:33 +1200

    Update drivers.js

--- routes/drivers.js
+++ routes/drivers.js
@@ -5,7 +5,7 @@ var db = mongojs("mongodb://alexey:alexey@ds121483.mlab.com:21483/lucky_ride", [
 
 
 //Get Single Driver
-router.get("/api/driver/:id", function(req, res, next){
+router.get("/driver/:id", function(req, res, next){
     db.drivers.find({_id: mongojs.ObjectId(req.params.id)},function(err, driver){
         if (err){
             res.send(err, req.params.id);
