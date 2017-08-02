af7eef9cc6c339a530fbfecd6ab7d9ec656d1152
alexeyomelchenko <alexeyomelchenko@catalyst.net.nz>
2017-08-02 21:41:23 +1200

    add driver endpoint

---
+++ routes/drivers.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var mongojs = require("mongojs");
+var db = mongojs("mongodb://alexey:alexey@ds121483.mlab.com:21483/lucky_ride", ["drivers"])
+
+
+//Get Single Driver
+router.get("/driver/:id", function(req, res, next){
+    db.drivers.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, driver){
+        if (err){
+            res.send(err);
+        }
+        res.send(driver);
+    });
+});
+
+module.exports = router;
